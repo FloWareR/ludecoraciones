2e8cc99b97dab48a7dbb5225b68ce7093e6eecab
Refered <jta.ordo@gmail.com>
2024-10-10 11:49:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,39 @@
 <?php
 
 function printArray($data){
-	echo '<pre'.print_r($data,true).'</pre>';
+	echo '<pre>'.print_r($data, true).'</pre>';
 }
 
-	ini_set("display_errors",1);
-	ini_set("display_startup_errors",1);
-	include("config/config.php");
-	include("config/autoload.php");
+ini_set("display_errors",1);
+ini_set("display_startup_errors",1);
+include("config/config.php");
+include("config/autoload.php");
 
-	$uri = $_SERVER["REQUEST_URI"];
-	if ($uri == "/"){
-		$uri = "";
-	}
-	$uritmp = explode("/",$uri);
-	array_shift($uritmp);
-	$uritmp = array_values($uritmp);
-	//printArray($uritmp);
-	$uri = str_replace("/ludecoraciones","",$uri);
-	if($uritmp[0] == ""){
-		$section = "index";
-	}else{
-		$section = $uritmp[0];
-	}
-	$filename = "views/sections/index.php";
-	if(file_exists($filename) and is_file($filename)){
-		include("views/general/top.php");
-		include($filename);
-		include("views/general/bottom.php");
+$uri = $_SERVER["REQUEST_URI"];
+if ($uri == "/"){
+	$uri = "";
+}
 
-	}else{echo 'error';}
+$uritmp = explode("/", $uri);
+array_shift($uritmp);
+$uritmp = array_values($uritmp); // Re-index the array
+
+//printArray($uritmp);
+$uri = str_replace("/ludecoraciones", "", $uri);
+
+// Check if $uritmp has at least one element
+if (isset($uritmp[0]) && $uritmp[0] === "") {
+	$section = "index";
+} else {
+	$section = isset($uritmp[0]) ? $uritmp[0] : "index";
+}
+
+$filename = "views/sections/index.php";
+if (file_exists($filename) && is_file($filename)) {
+	include("views/general/top.php");
+	include($filename);
+	include("views/general/bottom.php");
+} else {
+	echo 'error';
+}
 ?>
-
