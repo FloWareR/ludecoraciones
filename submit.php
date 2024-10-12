d8e3c6a982ff1f3f1d28c08ce9583e4871323de1
Refered <jta.ordo@gmail.com>
2024-10-12 11:32:29 -0600

    c

--- submit.php
+++ submit.php
@@ -2,27 +2,22 @@
 include('config/config.php'); 
 include_once('libraries/Database.php'); 
 
-// Instanciar la conexión a la base de datos
 $db = new Database();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Recoger los datos del formulario
     $name = trim($_POST['nombre']);
-    $email = trim($_POST['email']);  // Cambiado 'correo' por 'email'
+    $email = trim($_POST['email']); 
     $message = trim($_POST['mensaje']);
 
-    // Validar que no estén vacíos
     if (!empty($name) && !empty($email) && !empty($message)) {
         $sql = "INSERT INTO contacto (nombre, correo, mensaje) VALUES (:nombre, :correo, :mensaje)";
         
-        // Preparar los parámetros
         $params = [
             ':nombre' => $name,
-            ':correo' => $email,  // Cambiado el parámetro a 'email'
+            ':correo' => $email,  
             ':mensaje' => $message
         ];
         
-        // Ejecutar la consulta utilizando el método InsertSql
         $result = $db->InsertSql($sql, $params);
         
         if ($result['success']) {
