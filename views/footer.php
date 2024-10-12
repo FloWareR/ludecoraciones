178cbbe4e65f7629307e9e5cd4561a160242b45f
Refered <jta.ordo@gmail.com>
2024-10-12 16:31:20 -0600

    Facebook en el footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p>Conéctate con nosotros: 
-        <a href="#">Facebook</a> | 
+        <a href="https://www.facebook.com/ludecoraciones.814610">Facebook</a> | 
         <a href="https://www.instagram.com/lupita27ord/">Instagram</a>
     </p>
     <p>Dirección: Villahermosa, Tabasco | Tel: 9931602365</p>
