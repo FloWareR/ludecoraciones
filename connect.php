874473d8eae5b26a8c37787a5a89aa9228d2c450
Refered <jta.ordo@gmail.com>
2024-10-15 08:34:40 -0600

    DB connect tryout for flutter

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$conectar = mysqli_connect('localhost','u170629521_ReferedFlutter	
+','4VoTO/BMf!');
+$bd = mysqli_select_db($conectar, 'u170629521_ReferedFlutter')
+
+if($conectar){
+    echo 'bien';
+}
+if($bd){
+    echo 'bien base'
+}
+?>
