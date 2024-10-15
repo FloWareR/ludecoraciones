d573fbc21f2f319372f18c61f68cb3e3cf2195e9
Refered <jta.ordo@gmail.com>
2024-10-15 08:48:06 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,12 +2,12 @@
 
 
 $conectar = mysqli_connect('localhost','u170629521_ReferedFlutter','4VoTO/BMf!');
-//$bd = mysqli_select_db($conectar, 'u170629521_flutter')
+$db = mysqli_select_db($conectar, 'u170629521_flutter')
 
 if($conectar){
     echo 'bien';
 }
-if($bd){
+if($db){
     echo 'bien base';
 }
 ?>
