ac808d820167717314222671b6a37420abb3a92f
Refered <jta.ordo@gmail.com>
2024-11-02 17:35:19 -0600

    Update api.php

--- flutter/api.php
+++ flutter/api.php
@@ -1,15 +1,12 @@
 <?php
 header("Content-Type: application/json");
 
-// Configuración de conexión
+// Configuración de conexión a la base de datos
 $servername = "localhost";
 $username = "u170629521_ReferedFlutter";
 $password = "Refered1234";
 $dbname = "u170629521_flutter";
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    echo json_encode(["error" => "Método no permitido"]);
-    exit();
-}
+
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -18,22 +15,28 @@ try {
     exit();
 }
 
-// Verifica si el método es POST
+// Verifica que la solicitud sea POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     echo json_encode(["error" => "Método no permitido"]);
     exit();
 }
 
-// Decodifica el JSON recibido
+// Decodifica los datos JSON recibidos
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Verifica si se está registrando o iniciando sesión
+if ($data === null) {
+    echo json_encode(["error" => "Error al decodificar JSON"]);
+    exit();
+}
+
+// Verifica la acción a realizar: registro o inicio de sesión
 if (isset($data['action']) && $data['action'] === 'register') {
-    // Lógica para el registro de usuarios
+    // Lógica para registrar un nuevo usuario
     if (isset($data['usuario'], $data['correo'], $data['contrasena'])) {
         $sql = "INSERT INTO usuarios (usuario, correo, contrasena, telefono, edad) 
                 VALUES (:usuario, :correo, :contrasena, :telefono, :edad)";
 
+        // Hashea la contraseña
         $hashedPassword = password_hash($data['contrasena'], PASSWORD_BCRYPT);
 
         $stmt = $pdo->prepare($sql);
@@ -54,7 +57,7 @@ if (isset($data['action']) && $data['action'] === 'register') {
         echo json_encode(["error" => "Faltan datos obligatorios para el registro"]);
     }
 } elseif (isset($data['action']) && $data['action'] === 'login') {
-    // Lógica para el inicio de sesión de usuarios
+    // Lógica para iniciar sesión de usuario
     if (isset($data['correo'], $data['contrasena'])) {
         $sql = "SELECT contrasena FROM usuarios WHERE correo = :correo";
         $stmt = $pdo->prepare($sql);
@@ -63,6 +66,7 @@ if (isset($data['action']) && $data['action'] === 'register') {
         if ($stmt->rowCount() > 0) {
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
             
+            // Verifica la contraseña
             if (password_verify($data['contrasena'], $user['contrasena'])) {
                 echo json_encode(["success" => true, "message" => "Inicio de sesión exitoso"]);
             } else {
