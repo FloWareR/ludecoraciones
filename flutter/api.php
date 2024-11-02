b201bb6d9c198b566719419dedce04c74574d829
Refered <jta.ordo@gmail.com>
2024-11-02 17:53:34 -0600

    Update api.php

--- flutter/api.php
+++ flutter/api.php
@@ -1,7 +1,6 @@
 <?php
 header("Content-Type: application/json");
 
-// Configuración de conexión a la base de datos
 $servername = "localhost";
 $username = "u170629521_ReferedFlutter";
 $password = "Refered1234";
@@ -15,13 +14,11 @@ try {
     exit();
 }
 
-// Verifica que la solicitud sea POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     echo json_encode(["error" => "Método no permitido"]);
     exit();
 }
 
-// Decodifica los datos JSON recibidos
 $data = json_decode(file_get_contents("php://input"), true);
 
 if ($data === null) {
@@ -29,16 +26,11 @@ if ($data === null) {
     exit();
 }
 
-// Verifica la acción a realizar: registro o inicio de sesión
 if (isset($data['action']) && $data['action'] === 'register') {
-    // Lógica para registrar un nuevo usuario
     if (isset($data['usuario'], $data['correo'], $data['contrasena'])) {
         $sql = "INSERT INTO usuarios (usuario, correo, contrasena, telefono, edad) 
                 VALUES (:usuario, :correo, :contrasena, :telefono, :edad)";
-
-        // Hashea la contraseña
         $hashedPassword = password_hash($data['contrasena'], PASSWORD_BCRYPT);
-
         $stmt = $pdo->prepare($sql);
 
         try {
@@ -57,16 +49,13 @@ if (isset($data['action']) && $data['action'] === 'register') {
         echo json_encode(["error" => "Faltan datos obligatorios para el registro"]);
     }
 } elseif (isset($data['action']) && $data['action'] === 'login') {
-    // Lógica para iniciar sesión de usuario
     if (isset($data['correo'], $data['contrasena'])) {
         $sql = "SELECT contrasena FROM usuarios WHERE correo = :correo";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([':correo' => $data['correo']]);
-        
+
         if ($stmt->rowCount() > 0) {
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            
-            // Verifica la contraseña
             if (password_verify($data['contrasena'], $user['contrasena'])) {
                 echo json_encode(["success" => true, "message" => "Inicio de sesión exitoso"]);
             } else {
