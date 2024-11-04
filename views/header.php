a96dae1ddb6310fbed0bb49aa43fbb06bb3cfe80
Refered <jta.ordo@gmail.com>
2024-11-03 21:05:21 -0600

    Update header.php

--- views/header.php
+++ views/header.php
@@ -8,10 +8,10 @@
     </div>
     <nav>
         <ul class="nav-links">
-            <li><a href="/ludecoraciones">Inicio</a></li>
-            <li><a href="/views/sections/proyects.php">Proyectos</a></li>
-            <li><a href="/views/sections/quote.php">Cotizar</a></li>
-            <li><a href="/views/sections/about.php">Conoce LU</a></li> 
+            <li><a href="ludecoraciones.com">Inicio</a></li>
+            <li><a href="ludecoraciones.com/views/sections/proyects.php">Proyectos</a></li>
+            <li><a href="ludecoraciones.com/views/sections/quote.php">Cotizar</a></li>
+            <li><a href="ludecoraciones.com/views/sections/about.php">Conoce LU</a></li> 
         </ul>
     </nav>
 </header>
