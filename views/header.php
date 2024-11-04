1d6155c35ef38081b07e4a547ed87d988bff720f
Refered <jta.ordo@gmail.com>
2024-11-03 21:16:55 -0600

    App

--- views/header.php
+++ views/header.php
@@ -1,17 +1,16 @@
 <header class="navbar">
-    
     <div class="logo">
-        <img src="/Proyecto/assets/images/logo.png" alt="Logo de LU Decoraciones">
+        <img src="/assets/images/logo.png" alt="Logo de LU Decoraciones">
     </div>
     <div>
         <h1>LuDecoraciones</h1>
     </div>
     <nav>
         <ul class="nav-links">
-            <li><a href="ludecoraciones.com">Inicio</a></li>
-            <li><a href="ludecoraciones.com/views/sections/proyects.php">Proyectos</a></li>
-            <li><a href="landingpage/views/sections/quote.php">Cotizar</a></li>
-            <li><a href="landingpage/views/sections/about.php">Conoce LU</a></li> 
+            <li><a href="/index.php">Inicio</a></li>
+            <li><a href="/views/sections/proyects.php">Proyectos</a></li>
+            <li><a href="/views/sections/quote.php">Cotizar</a></li>
+            <li><a href="/views/sections/about.php">Conoce LU</a></li> 
         </ul>
     </nav>
 </header>
