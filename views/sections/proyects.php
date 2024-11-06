48230f97a0563a0b8fcc4bff51372ccd8fddcefe
Refered <jta.ordo@gmail.com>
2024-11-05 19:20:32 -0600

    Borre lo que no use de flutter

---
+++ views/sections/proyects.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nosotros - LU Decoraciones</title>
+    <link rel="stylesheet" href="../../assets/css/styles.css"> 
+</head>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/views/header.php'); ?>
+
+    <main>
+
+    <div class="gallery">
+            <div class="card tall">
+                <img src="/Proyecto/assets/images/img3.jpeg" alt="Cumpleaños con globos morados">
+                <div class="caption">Cumpleaños con globos morados</div>
+            </div>
+            <div class="card">
+                <img src="/Proyecto/assets/images/img4.jpeg" alt="Cumpleaños colores vibrantes">
+                <div class="caption">Cumpleaños colores vibrantes</div>
+            </div>
+            <div class="card">
+                <img src="/Proyecto/assets/images/img5.jpeg" alt="Graduación escolar">
+                <div class="caption">Graduación escolar</div>
+    </div>
+
+
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/views/footer.php'); ?>
+</body>
+</html>
