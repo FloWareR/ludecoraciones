9b6e85f54d645f15843738d4db767956d19b1e7a
Refered <jta.ordo@gmail.com>
2024-11-05 19:24:47 -0600

    Update proyects.php

--- views/sections/proyects.php
+++ views/sections/proyects.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre Nosotros - LU Decoraciones</title>
-    <link rel="stylesheet" href="../../assets/css/styles.css"> 
+    <link rel="stylesheet" href="/assets/css/styles.css"> 
 </head>
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/views/header.php'); ?>
