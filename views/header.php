79e5c21f66a5cfd5ed60857b97e3e338a1ca19aa
Refered <jta.ordo@gmail.com>
2024-11-13 21:15:48 -0600

    Update header.php

--- views/header.php
+++ views/header.php
@@ -7,7 +7,7 @@
         <ul class="nav-links">
             <li><a href="https://ludecoraciones.com/">Inicio</a></li>
             <li><a href="/views/sections/quote.php">Cotizar</a></li>
-            <li><a href="/views/sections/proyects.php">Cotizar</a></li>
+            <li><a href="/views/sections/proyects.php">Proyectos</a></li>
             <li><a href="/views/sections/about.php">Conoce LU</a></li> 
 
         </ul>
