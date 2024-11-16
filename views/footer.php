dc74828c897b155a9e9a2a4f16e9f4421262c8fa
Refered <jta.ordo@gmail.com>
2024-11-16 17:06:23 -0600

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <p>Conéctate con nosotros: 
-        <a href="https://www.facebook.com/tuPagina" target="_blank" rel="noopener noreferrer">
-            <img src="/assets/images/facebook-icon.png" alt="Facebook" class="social-icon">
+        <a href="https://www.facebook.com/ludecoraciones.814610" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/images/Facebook.png" alt="Facebook" class="social-icon">
         </a>
     </p>
     <p>Dirección: Villahermosa, Tabasco | Tel: 9931602365</p>
