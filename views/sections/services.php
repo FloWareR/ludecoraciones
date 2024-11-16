8d0bd552cad53e2de07f5726d478ae00882a1c42
Refered <jta.ordo@gmail.com>
2024-11-16 16:25:28 -0600

    assa

--- views/sections/services.php
+++ views/sections/services.php
@@ -17,14 +17,14 @@
             <p class="description">Ya sea para un evento pequeño o un evento corporativo, tenemos la experiencia y los recursos para hacerlo posible.</p>
             <div class="service-cards">
                 <!-- Tarjeta 1 -->
-                <a href="/views/quote.php" class="service-item">
+                <a href="/views/sections/quote.php" class="service-item">
                     <img src="/assets/images/img6.jpeg" alt="Decoración de Eventos">
                     <h3>Decoración de Eventos</h3>
                     <p>Ofrecemos un servicio completo de decoración para bodas, cumpleaños, y eventos especiales.</p>
                 </a>
                 
                 <!-- Tarjeta 2 -->
-                <a href="/views/quote.php" class="service-item">
+                <a href="/views/sections/quote.php" class="service-item">
                     <img src="/assets/images/img11.jpeg" alt="Renta de Inventario">
                     <h3>Renta de Inventario</h3>
                     <p>Renta de mobiliario como letreros LED, mamparas circulares, estructuras de herrería, mariposas con luces, número 15 con luces, caja de barbie, entre otros.</p>
