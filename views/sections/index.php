368180ebc4a4083efc31e6836a9dfae908e305fd
Refered <jta.ordo@gmail.com>
2024-11-16 14:03:50 -0600

    Update index.php

--- views/sections/index.php
+++ views/sections/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LuDecoraciones</title>
+    <link rel="icon" href="/images/logo.png" type="image/png">
+
     <link rel="stylesheet" href="assets/css/styles.css"> 
 </head>
 <body>
