572e0738bf080b284ec024ccae43d534667be1e2
Refered <jta.ordo@gmail.com>
2024-11-16 16:51:58 -0600

    a

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,8 @@
 <footer>
     <p>Conéctate con nosotros: 
-        <a href="https://www.facebook.com/ludecoraciones.814610">Facebook</a> | 
+        <a href="https://www.facebook.com/ludecoraciones.814610">
+        <img src="/assets/images/Facebook.png" alt="Facebook Logo">
+        </a> 
         <a href="https://www.instagram.com/lupita27ord/">Instagram</a>
     </p>
     <p>Dirección: Villahermosa, Tabasco | Tel: 9931602365</p>
--- views/sections/services.php
+++ views/sections/services.php
@@ -16,14 +16,12 @@
             <h2>Nuestros Servicios</h2>
             <p class="description">Ya sea para un evento pequeño o un evento corporativo, tenemos la experiencia y los recursos para hacerlo posible.</p>
             <div class="service-cards">
-                <!-- Tarjeta 1 -->
                 <a href="/views/sections/quote.php" class="service-item">
                     <img src="/assets/images/img6.jpeg" alt="Decoración de Eventos">
                     <h3>Decoración de Eventos</h3>
                     <p>Ofrecemos un servicio completo de decoración para bodas, cumpleaños, y eventos especiales.</p>
                 </a>
                 
-                <!-- Tarjeta 2 -->
                 <a href="/views/sections/quote.php" class="service-item">
                     <img src="/assets/images/img11.jpeg" alt="Renta de Inventario">
                     <h3>Renta de Inventario</h3>
