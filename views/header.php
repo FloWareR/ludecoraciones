90c5925c222d4b63d9df2e79c77fdc3c00da0331
Refered <jta.ordo@gmail.com>
2024-11-17 16:36:07 -0600

    navbar

--- views/header.php
+++ views/header.php
@@ -1,16 +1,31 @@
-
+<script>
+function toggleDrawer() {
+    const drawer = document.getElementById('drawer');
+    drawer.classList.toggle('open');
+}
+</script>
 <div class="navbar">
     <div class="logo">
         <img src="/assets/images/logo.png" alt="Logo">
     </div>
+    <button class="hamburger-btn" onclick="toggleDrawer()">
+        ☰
+    </button>
     <nav id="drawer" class="drawer-nav">
-        <ul class="nav-links">
+        <button class="close-btn" onclick="toggleDrawer()">✖</button>
+        <ul class="nav-links drawer-links">
             <li><a href="https://ludecoraciones.com/">Inicio</a></li>
-            <li><a href="/views/sections/proyects.php">Galeria</a></li>
+            <li><a href="/views/sections/proyects.php">Galería</a></li>
             <li><a href="/views/sections/services.php">Servicios</a></li>
-
             <li><a href="/views/sections/quote.php">Cotizar</a></li>
             <li><a href="/views/sections/about.php">Conoce LU</a></li>
         </ul>
     </nav>
+    <ul class="nav-links desktop-links">
+        <li><a href="https://ludecoraciones.com/">Inicio</a></li>
+        <li><a href="/views/sections/proyects.php">Galería</a></li>
+        <li><a href="/views/sections/services.php">Servicios</a></li>
+        <li><a href="/views/sections/quote.php">Cotizar</a></li>
+        <li><a href="/views/sections/about.php">Conoce LU</a></li>
+    </ul>
 </div>
