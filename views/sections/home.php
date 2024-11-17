865d0c29491fad4f4186a1ec00326bdcded0d02d
Refered <jta.ordo@gmail.com>
2024-11-17 16:11:07 -0600

    Update home.php

--- views/sections/home.php
+++ views/sections/home.php
@@ -9,7 +9,9 @@
         "assets/images/img6.jpeg",
         "assets/images/img7.jpeg",
         "assets/images/img8.jpeg",
-        "assets/images/img9.jpeg"
+        "assets/images/img9.jpeg",
+        "assets/images/img12.jpeg",
+        "assets/images/img13.jpeg"
     ];
 
     function updateCarousel() {
