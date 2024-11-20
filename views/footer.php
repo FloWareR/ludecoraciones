900894d5f5e9221eaaab36a60d9e681a1b9772de
Refered <jta.ordo@gmail.com>
2024-11-20 07:31:49 -0600

    A

--- views/footer.php
+++ views/footer.php
@@ -3,7 +3,7 @@
             <img src="/assets/images/Facebook2.png" alt="Facebook" class="social-icon">
         </a>
         <a href="https://www.instagram.com/lupita27ord/" target="_blank" rel="noopener noreferrer">
-            <img src="/assets/images/Instagram.png" alt="Instagram" class="social-icon">
+            <img src="/assets/images/Instagram2.png" alt="Instagram" class="social-icon">
         </a>
     <p>Dirección: Villahermosa, Tabasco | Tel: 9931602365</p>
 </footer>
