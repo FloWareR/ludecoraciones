48130f95faa0a3cd5c99450910e57ffa24b995a1
Refered <jta.ordo@gmail.com>
2024-11-27 07:42:06 -0600

    Update quote.php

--- views/sections/quote.php
+++ views/sections/quote.php
@@ -8,14 +8,7 @@
     <link rel="stylesheet" href="../../assets/css/styles.css"> 
 </head>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-131FZTGBX8"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-131FZTGBX8');
-</script>
 <script>
 function abrirW() {
     const telefono = '9931602365';
