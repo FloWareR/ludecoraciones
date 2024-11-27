d2e1d10c4a797443ff4fb0eddf108a490c027c7c
Refered <jta.ordo@gmail.com>
2024-11-27 13:42:29 -0600

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,13 +1,14 @@
 <footer>
-    <div><a href="https://www.facebook.com/ludecoraciones.814610" target="_blank" rel="noopener noreferrer">
+    <div>
+        <a href="https://www.facebook.com/ludecoraciones.814610" target="_blank" rel="noopener noreferrer">
             <img src="/assets/images/Facebook2.png" alt="Facebook" class="social-icon">
         </a>
         <a href="https://www.instagram.com/lupita27ord/" target="_blank" rel="noopener noreferrer">
             <img src="/assets/images/Instagram2.png" alt="Instagram" class="social-icon">
         </a>
     </div>
-    <div><p>Tel: 9931602365</p>
-    <p>Dirección: Villahermosa, Tabasco</p>
-</div>
-    
+    <div>
+        <p>Tel: 9931602365</p>
+        <p>Dirección: Villahermosa, Tabasco</p>
+    </div>
 </footer>
