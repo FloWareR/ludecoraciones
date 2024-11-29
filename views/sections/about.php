9a0467b90d5de2093afe93fc839d7c13370d9b0f
Refered <jta.ordo@gmail.com>
2024-11-29 12:55:05 -0600

    Update about.php

--- views/sections/about.php
+++ views/sections/about.php
@@ -25,11 +25,6 @@
             <div class="about-text">
                 <h2>Sobre Nosotros</h2>
                 <p>LU Decoraciones es una empresa de decoraciones fundada por Lupita Ordoñez, una talentosa y experimentada decoradora de Tabasco. Con una sólida formación respaldada por múltiples certificaciones en el área de decoraciones y varios años de experiencia, Lupita ha convertido su pasión por el diseño en una empresa comprometida con transformar eventos en momentos memorables.
-
-Especializados en decoraciones con globos, LU Decoraciones se dedica a llevar color, creatividad y elegancia a una amplia gama de celebraciones, brindando un servicio a domicilio personalizado que garantiza que cada detalle refleje los deseos y la personalidad del cliente. Desde eventos íntimos hasta grandes celebraciones, Lupita se esfuerza por crear espacios ideales tanto para disfrutar del evento como para capturar el recuerdo perfecto en fotografías inolvidables.
-
-A lo largo de su carrera, Lupita ha trabajado en todo tipo de eventos, incluyendo fiestas infantiles, cumpleaños, quinceañeras y bodas, adaptando su estilo a la temática y necesidades de cada cliente. Con una atención meticulosa a los detalles, desde arcos de globos vibrantes hasta fondos elegantes para fotografías, LU Decoraciones busca superar las expectativas de cada cliente, convirtiendo cualquier espacio en una experiencia visual única.
-
             </div>
             <div class="about-image">
                 <img src="../../assets/images/img10.jpeg" alt="Lupita Ordoñez">
